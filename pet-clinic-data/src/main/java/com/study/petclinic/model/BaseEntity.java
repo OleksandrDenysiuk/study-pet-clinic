361164cca2eb9d2c525f5e8d101ca84e94fecc3c
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-16 18:58:32 +0300

    Implement Map Based Services

--- pet-clinic-data/src/main/java/com/study/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/study/petclinic/model/BaseEntity.java
@@ -2,10 +2,15 @@ package com.study.petclinic.model;
 
 import java.io.Serializable;
 
-
 public class BaseEntity implements Serializable {
 
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
