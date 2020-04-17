cffe2d19ccc82e3f527f35692ba75f13b8198d92
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-17 12:20:48 +0300

    Added vets list index page. Closed #1

--- pet-clinic-web/src/main/java/com/study/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/study/petclinic/controller/VetController.java
@@ -1,5 +1,6 @@
 package com.study.petclinic.controller;
 
+import com.study.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping("/vets")
     public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
