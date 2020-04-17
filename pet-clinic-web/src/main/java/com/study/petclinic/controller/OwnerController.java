de5b1a7b4f845f0ac5f474f7275539d6de70e949
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-17 12:16:00 +0300

    Added owner list index page. Closed #14

--- pet-clinic-web/src/main/java/com/study/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/study/petclinic/controller/OwnerController.java
@@ -1,5 +1,6 @@
 package com.study.petclinic.controller;
 
+import com.study.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners")
     public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
