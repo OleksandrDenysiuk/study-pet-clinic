8b68bf665ca431431a6b901d1f4d3db03d4af011
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-20 10:06:52 +0300

    Converted to JPA Entities. Closes #19

--- pet-clinic-data/src/main/java/com/study/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/study/petclinic/model/Pet.java
@@ -1,13 +1,30 @@
 package com.study.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_it")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
